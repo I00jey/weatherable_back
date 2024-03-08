6a2982d7b1ab03515fe5c2b053266e5d2ad56d76
JHSasdf <sgh4760@naver.com>
2024-03-08 13:58:29 +0900

    feat_connecting_mysql_ec2

--- weatherable/src/main/java/com/weatherable/weatherable/Controller/ClosetController.java
+++ weatherable/src/main/java/com/weatherable/weatherable/Controller/ClosetController.java
@@ -1,14 +1,30 @@
 package com.weatherable.weatherable.Controller;
 
+import com.weatherable.weatherable.Entity.ClosetEntity;
+import com.weatherable.weatherable.Repository.ClosetRepository;
 import com.weatherable.weatherable.Service.ClosetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class ClosetController {
 
     @Autowired
     ClosetService closetService;
 
+
+
+    @GetMapping("/")
+    List<ClosetEntity> getClosetByUserid(@RequestParam Long userIndex) {
+        System.out.println("sfsfsfefe"+userIndex);
+        List<ClosetEntity> result = closetService.getAllList(userIndex);
+        System.out.println(result);
+        return result;
+    }
 
 }
