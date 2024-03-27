041b88b2a5eb1e0e2fe8d61164a8c9721db986bb
dydgjs200 <dydgjs2016@naver.com>
2024-03-27 13:57:23 +0900

    ddd

--- weatherable/src/main/java/com/weatherable/weatherable/Config/WebMvcConfig.java
+++ weatherable/src/main/java/com/weatherable/weatherable/Config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 //                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://43.201.22.187:3000")
+                .allowedOrigins("https://43.201.22.187:3000")
                 .allowedMethods("GET","POST","PUT","DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
