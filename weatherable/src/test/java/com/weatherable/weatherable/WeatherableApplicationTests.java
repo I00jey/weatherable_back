d0e747dacbc0b983a3eb12398da18678f847fb64
I00jey <cyj5056@gmail.com>
2024-03-27 21:18:29 +0900

    서버 배포 cors 재설정

--- weatherable/src/test/java/com/weatherable/weatherable/WeatherableApplicationTests.java
+++ weatherable/src/test/java/com/weatherable/weatherable/WeatherableApplicationTests.java
@@ -4,11 +4,11 @@ import com.weatherable.weatherable.Repository.ClosetRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WeatherableApplicationTests {
-	@Test
-	void contextLoads() {
-	}
-
-}
+//
+//@SpringBootTest
+//class WeatherableApplicationTests {
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
