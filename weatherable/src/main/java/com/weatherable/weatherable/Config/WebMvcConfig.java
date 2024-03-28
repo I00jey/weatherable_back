ab13b2c3cdd0d39c1e4a149c05b89491700ea722
I00jey <cyj5056@gmail.com>
2024-03-28 12:26:59 +0900

    서버 배포 cors 재설정

--- weatherable/src/main/java/com/weatherable/weatherable/Config/WebMvcConfig.java
+++ weatherable/src/main/java/com/weatherable/weatherable/Config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 //                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://3.36.128.237:3000")
+                .allowedOrigins("http://3.36.128.237")
                 .allowedMethods("GET","POST","PUT","DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
